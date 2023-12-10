94a2114fd15067f95d3202cd63434aba06173b65
djavo666aca <52077531+djavo666aca@users.noreply.github.com>
2023-12-10 16:41:19 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Property
+{
+    class Storage {
+        public string NekiString
+        { get; set; }
+
+        public int NekiInt
+        { get; set;}
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Storage s = new Storage();
+            
+            s.NekiString = "Neki string";
+            s.NekiInt = 1;
+
+            Console.WriteLine(s.NekiString);
+            Console.WriteLine(s.NekiInt);
+        }
+    }
+}
